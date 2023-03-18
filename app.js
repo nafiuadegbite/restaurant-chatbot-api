6492e57ae9926f45d49d2d7973271a12dfc5f4eb
Nafiu Adegbite <youngguru23@gmail.com>
2023-03-18 21:09:57 +0100

    fix: vercel file

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use("/api/v1", api);
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
+  res.sendFile(path.join(__dirname, "client"));
 });
 
 module.exports = { app };
